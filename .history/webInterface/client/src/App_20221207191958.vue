95b8f08114d71b8ba404987568bf0338fa66368b
Taotaoop <103357080+Taotaoop@users.noreply.github.com>
2022-12-07 19:24:25 -0600

    small updata in main page

--- .history/webInterface/client/src/App_20221207191958.vue
+++ .history/webInterface/client/src/App_20221207191958.vue
@@ -8,9 +8,9 @@
         </div> -->
         <h1>Car Web</h1>
         
-        <el-menu class="el-menu-demo" mode="horizontal"
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect"
           background-color="#4799e6aa" text-color="#333" active-text-color="#ffd04b">
-          <el-menu-item index="1"><router-link to="/">Home</router-link></el-menu-item>
+          <el-menu-item index="1"><router-link to="/Home">Home</router-link></el-menu-item>
           <el-menu-item index="2"><router-link to="/Update">Updata information</router-link></el-menu-item>
           <el-menu-item index="3" ><router-link to="/AddCar">Add car</router-link></el-menu-item>
           <el-menu-item index="4" ><router-link to="/QueryByVim">Query By Vim</router-link></el-menu-item>
--- .history/webInterface/client/src/App_20221207192221.vue
+++ .history/webInterface/client/src/App_20221207192221.vue
@@ -10,7 +10,7 @@
         
         <el-menu class="el-menu-demo" mode="horizontal"
           background-color="#4799e6aa" text-color="#333" active-text-color="#ffd04b">
-          <el-menu-item index="1"><router-link to="/">Home</router-link></el-menu-item>
+          <el-menu-item index="1"><router-link to="/Home">Home</router-link></el-menu-item>
           <el-menu-item index="2"><router-link to="/Update">Updata information</router-link></el-menu-item>
           <el-menu-item index="3" ><router-link to="/AddCar">Add car</router-link></el-menu-item>
           <el-menu-item index="4" ><router-link to="/QueryByVim">Query By Vim</router-link></el-menu-item>
